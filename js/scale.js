d36b681559d2b1c46acc91e38776c1152152d5f3
hoàng lợi <ilehoe0902@gmail.com>
2024-11-21 05:29:23 +0700

    update

--- js/scale.js
+++ js/scale.js
@@ -10,22 +10,19 @@ function scalePage() {
     
     //add top;
     const firstHomepage = document.querySelector('.first-homepage');
-    const originalWidth = 1440; // Chiều rộng màn hình thiết kế
-    const originalHeight = 900; // Chiều cao màn hình thiết kế
-    const scaleRatio = viewportWidth / originalWidth;
-    const scaledHeight = originalHeight * scaleRatio;
-    const bottomOffset = scaledHeight - window.innerHeight;
-    firstHomepage.style.top = `calc(-${bottomOffset}px)`;
 
-    // if (firstHomepage) {
-    //   if (scaleFactor !== 1) {
-    //     const topValue = (900 * scaleFactor - viewportHeight) * 0.8;
-    //     firstHomepage.style.top = `-${topValue}px`; // Đảm bảo thêm đơn vị "px"
-    //   }
-    //     //   } else {
-    // //     // Xử lý trường hợp scaleFactor = 1 (ví dụ: đặt lại về giá trị mặc định)
-    // //     //firstHomepage.style.top = '0px'; // Hoặc giá trị top mặc định khác
-    //    }
+    if (firstHomepage) {
+      if (scaleFactor !== 1) {
+        const vipRatio = 1.883;
+        const currentRatio = viewportWidth/viewportHeight;
+        const vvip = currentRatio / vipRatio;
+        const topValue = 136*vvip*vvip*vvip;
+        firstHomepage.style.top = `-${topValue}px`; // Đảm bảo thêm đơn vị "px"
+      }
+        //   } else {
+    //     // Xử lý trường hợp scaleFactor = 1 (ví dụ: đặt lại về giá trị mặc định)
+    //     //firstHomepage.style.top = '0px'; // Hoặc giá trị top mặc định khác
+       }
   }
   
   window.addEventListener('resize', scalePage);
