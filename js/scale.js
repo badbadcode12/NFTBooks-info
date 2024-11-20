7b10812fd29e13ad5f664ac5fe81339003fca30a
hoàng lợi <ilehoe0902@gmail.com>
2024-11-20 22:58:56 +0700

    update

--- js/scale.js
+++ js/scale.js
@@ -3,6 +3,7 @@ function scalePage() {
     //const viewportWidth = window.innerWidth;
     //document.documentElement.clientWidth
     const viewportWidth = document.documentElement.clientWidth;
+    const viewportHeight = document.documentElement.clientHeight;
     const scaleFactor = Math.max(viewportWidth / 1440,1).toFixed(3);
     document.body.style.transform = `scale(${scaleFactor})`;
     document.body.style.transformOrigin = 'top left';
@@ -12,7 +13,7 @@ function scalePage() {
 
     if (firstHomepage) {
       if (scaleFactor !== 1) {
-        const topValue = (900 * scaleFactor - 900) * 0.6;
+        const topValue = (900 * scaleFactor - viewportHeight) * 0.8;
         firstHomepage.style.top = `-${topValue}px`; // Đảm bảo thêm đơn vị "px"
       }
         //   } else {
