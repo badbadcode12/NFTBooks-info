2d1d71b42261979bdff9be1a4ac9937b89b891fa
hoàng lợi <ilehoe0902@gmail.com>
2024-11-20 16:15:29 +0700

    add custom top first intro

--- js/scale.js
+++ js/scale.js
@@ -6,6 +6,19 @@ function scalePage() {
     const scaleFactor = Math.max(viewportWidth / 1440,1).toFixed(3);
     document.body.style.transform = `scale(${scaleFactor})`;
     document.body.style.transformOrigin = 'top left';
+    
+    //add top;
+    const firstHomepage = document.querySelector('.first-homepage');
+
+    if (firstHomepage) {
+      if (scaleFactor !== 1) {
+        const topValue = (900 * scaleFactor - 900) * 0.6;
+        firstHomepage.style.top = `-${topValue}px`; // Đảm bảo thêm đơn vị "px"
+      }
+        //   } else {
+    //     // Xử lý trường hợp scaleFactor = 1 (ví dụ: đặt lại về giá trị mặc định)
+    //     //firstHomepage.style.top = '0px'; // Hoặc giá trị top mặc định khác
+       }
   }
   
   window.addEventListener('resize', scalePage);
