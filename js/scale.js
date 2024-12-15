d70d104bca4d1f42ed2d09e18c2e4c00c10967b1
hoàng lợi <ilehoe0902@gmail.com>
2024-12-15 17:49:45 +0700

    add investor translaotr how-to-buy page

--- js/scale.js
+++ js/scale.js
@@ -2,15 +2,18 @@
 function scalePage() {
     //const viewportWidth = window.innerWidth;
     //document.documentElement.clientWidth
+    const firstAuthor = document.querySelector("#first-author > div");
     const viewportWidth = document.documentElement.clientWidth;
     const viewportHeight = document.documentElement.clientHeight;
     const scaleFactor = Math.max(viewportWidth / 1440,1).toFixed(3);
-    document.body.style.transform = `scale(${scaleFactor})`;
-    document.body.style.transformOrigin = 'top left';
-    
+    // document.body.style.transform = `scale(${scaleFactor})`;
+    // document.body.style.transformOrigin = 'top left';
+    firstAuthor.style.transform = `scale(${scaleFactor})`;
+    firstAuthor.style.transformOrigin = 'top left';
+    const firstAuthorH1 = document.querySelector('div.welcome-title-author');
+    firstAuthorH1.style.transform = `scale(${1/scaleFactor})`;
     //add top;
     const firstHomepage = document.querySelector('.first-homepage');
-
     if (firstHomepage) {
       if (scaleFactor !== 1) {
         const scaleDiv = 900 - (viewportHeight/scaleFactor);
